b02e05f5e2dce663a88a7f451b7d0a756930511b
Arthur <cotikgg@gmail.com>
2026-01-02 12:28:53 +0100

    feat: buger menu activatio

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const burgerMenu = document.querySelector('.burger__menu');
 const burgerBtn = document.querySelector('.nav__burger');
 
 burgerBtn.addEventListener('click', () => {
-    if (burgerMenu.classList.contains('.burger__menu--active')) {
-        burgerMenu.classList.remove('.burger__menu--active');
+    if (burgerMenu.classList.contains('burger__menu--active')) {
+        burgerMenu.classList.remove('burger__menu--active');
     } else {
-        burgerMenu.classList.add('.burger__menu--active');
+        burgerMenu.classList.add('burger__menu--active');
     }
 });
