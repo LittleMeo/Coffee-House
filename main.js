5f3948d50224c9ad76a25e8bda68d29934a87ff9
Arthur <cotikgg@gmail.com>
2026-01-02 14:19:29 +0100

    feat: close menu after click on navItem

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-const burgerMenu = document.querySelector('.burger__menu');
-const burgerBtn = document.querySelector('.nav__burger');
+// querySelectors
+const burgerMenu = document.querySelector(".burger__menu");
+const burgerBtn = document.querySelector(".nav__burger");
+const navItems = document.querySelectorAll(".nav__item");
 
-burgerBtn.addEventListener('click', () => {
-    if (burgerMenu.classList.contains('burger__menu--active')) {
-        burgerMenu.classList.remove('burger__menu--active');
-    } else {
-        burgerMenu.classList.add('burger__menu--active');
-    }
+// functions
+const openBurger = () => {
+  burgerMenu.classList.add("burger__menu--active");
+};
+
+const closeBurger = () => {
+  burgerMenu.classList.remove("burger__menu--active");
+};
+
+// open and close burgerMenu
+burgerBtn.addEventListener("click", () => {
+  if (burgerMenu.classList.contains("burger__menu--active")) {
+    closeBurger();
+  } else {
+    openBurger();
+  }
+});
+
+// close burgerMenu clicked on nav-link
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    closeBurger();
+  });
 });
